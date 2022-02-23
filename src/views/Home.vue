c62d576bb58fe0e7c5928b4470180e94552959c3
amjadkhansarwar <muhammad.amjad@iths.se>
2022-02-23 09:51:29 +0100

    done some thind but not know

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,7 +11,7 @@
         type="checkbox"
         id="Unisex"
         name="Unisex"
-        value="Unisex"
+        value="unisex"
         v-model="categories"
       />
       <label for="Unisex">Unisex</label>
@@ -20,7 +20,7 @@
         type="checkbox"
         id="Male"
         name="Male"
-        value="Male"
+        value="male"
         v-model="categories"
       />
       <label for="Male">Male</label>
@@ -29,7 +29,7 @@
         type="checkbox"
         id="Female"
         name="Female"
-        value="Female"
+        value="female"
         v-model="categories"
       />
       <label for="Female">Female</label>
@@ -37,7 +37,7 @@
     {{ categories }}
 
     <h3>Filtreringens resultat</h3>
-    <p>{{ productsByFilter }}</p>
+    {{productsByFilter}}
   </div>
 </template>
 
