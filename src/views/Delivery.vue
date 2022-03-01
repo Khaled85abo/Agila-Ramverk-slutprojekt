27f45d4800275df8faa690349a376bcd2019a183
amjadkhansarwar <muhammad.amjad@iths.se>
2022-03-01 14:09:49 +0100

    Devlivery Design
    - Redisgn total delivery page same mokup

--- src/views/Delivery.vue
+++ src/views/Delivery.vue
@@ -1,71 +1,58 @@
 <template>
-  <div>
-    <div class="title">
-      <h2>Choose Delivery</h2>
-      <h2>Price</h2>
-    </div>
-    <br />
-    <hr />
-
-    <article>
-      <section>
-        <label class="container"
-          >Home delivery
+  <div class="delivery">
+    <div class="wrapper">
+      <section class="header">
+      <h1>Delivery</h1>
+      <h1>Price</h1>
+      </section>
+      <section class="main">
+        <article class="article">
+          <div class="radio-button">
+          <input type="radio" name="radio" class="radio" />
+          <div>
+          <h3> Home delivery</h3>
           <p>2-3 days or Express within 24h</p>
-          <input type="radio" name="radio" />
-          <span class="checkmark"></span>
-        </label>
-        <br />
-        <label class="container"
-          >Instabox
-          <p>1-2 days</p>
-          <input type="radio" name="radio" checked />
-          <span class="checkmark"></span>
-        </label>
-        <br />
-        <label class="container"
-          >Post Office (PostNord)
-          <p>3-5 days</p>
-          <input type="radio" name="radio" />
-          <span class="checkmark"></span>
-        </label>
+          </div>
+         </div>
+        <h2>5 $</h2>
+        </article>
+        <article class="article">
+          <div class="radio-button">
+            <input type="radio" name="radio" checked  class="radio"/>
+            <div>
+            <h2> Instabox </h2>
+            <p>1-2 days</p>
+            </div>
+          </div>
+         <h2 class="free-delivery">Free !</h2>
+        </article>
+        <article class="article">
+          <div class="radio-button">
+           <input type="radio" name="radio" class="radio"/>
+           <div>
+           <h2> Post Office (PostNord)</h2>
+           <p>3-5 days</p>
+           </div>
+          </div>
+        <h2>7 $</h2>
+        </article>
+          
+         
+          
       </section>
-
-      <section class="prices">
-        <div id="home-price">
-          <h2>5 $</h2>
-        </div>
-        <br />
-
-        <div id="instabox-price">
-          <h2>Free !</h2>
-        </div>
-        <br />
-
-        <div id="postNord-price">
-          <h2>7 $</h2>
-        </div>
-        <br />
-      </section>
-    </article>
-    <br />
-
-    <hr />
-
-    <br />
-    <div class="total">
-      <div class="totalPrice">
+      <section class="total-price">
         <h3>Total:{{ totalPrice }}</h3>
         <p>Down payment from only ? $/ month</p>
-      </div>
-      <br /><br />
-
-      <div class="stage">
+       </section>
+      <section class="button">
+      <div >
         <Btn @click="back">Back to cart</Btn>
+        </div><div>
         <Btn @click="shipping">Next - Shipping</Btn>
       </div>
+     
+    </section>
     </div>
-    <hr />
   </div>
 </template>
 
@@ -93,118 +80,58 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.title {
-  margin-top: 10px;
-  margin: 20px;
+.delivery{
   display: flex;
-  justify-content: space-around;
-  flex-direction: row;
-}
-
-article {
-  margin-top: 10px;
-  margin: 20px;
-  display: flex;
-  justify-content: space-around;
-  flex-direction: row;
-
-  section {
-    margin-top: 30px;
-    margin-right: 30px;
-  }
-
-  .container {
-    display: block;
-    position: relative;
-    padding-left: 35px;
-    margin-bottom: 12px;
-    font-size: 22px;
-    -webkit-user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
-    user-select: none;
-    margin-right: 30px;
-  }
-
-  .container input {
-    position: absolute;
-    opacity: 0;
-    cursor: pointer;
-  }
-
-  .checkmark {
-    position: absolute;
-    top: 0;
-    left: 0;
-    height: 25px;
-    width: 25px;
-    background-color: #eee;
-    border-radius: 50%;
-  }
-
-  .container:hover input ~ .checkmark {
-    background-color: rgba(0, 0, 0, 0.65);
-  }
-
-  .container input:checked ~ .checkmark {
-    background-color: black;
-  }
-
-  .checkmark:after {
-    content: "";
-    position: absolute;
-    display: none;
-  }
-
-  .container input:checked ~ .checkmark:after {
-    display: block;
-  }
-
-  .container .checkmark:after {
-    top: 9px;
-    left: 9px;
-    width: 8px;
-    height: 8px;
-    border-radius: 50%;
-    background: white;
-  }
-
-  .prices {
+  max-width: 100%;
+  justify-content: center;
+  align-items: center;
+} 
+.wrapper {
+    width: 80%;
     display: flex;
     flex-direction: column;
-    justify-content: space-between;
-  }
-
-  #home-price {
-    font-weight: bold;
-  }
-
-  #instabox-price {
-    font-weight: bold;
-    color: red;
-  }
-
-  #postNord-price {
-    font-weight: bold;
-  }
-}
-
-hr {
-  display: block;
-  border-top: 2px solid black;
-}
-
-.total {
-  margin: 30px;
-  display: flex;
-  flex-direction: column;
-
-  .totalPrice {
-    align-self: flex-end;
-  }
-  .stage {
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+    margin: 2rem;
+  .header, .button{
     display: flex;
+    flex-direction: row;
+    width: 100%;
     justify-content: space-between;
-  }
+  }  
 }
+.total-price{
+    display: flex;
+    flex-direction: column;
+    align-items: end;
+    align-self: stretch;
+  }
+  .main{
+    display:flex;
+    flex-direction: column;
+    border-top: 1px solid $pitchBlack;
+    border-bottom: 1px solid $pitchBlack;
+    width: 100%;
+    .article{
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+    }
+  }
+  .radio-button{
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    margin: 1rem;
+  }
+  .radio{
+    margin: 1rem;
+    width: 20px;
+    height: 20px;
+  }
+  .free-delivery{
+    color: $cartCount;
+  }
 </style>
